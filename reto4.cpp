8decaf76c85c98a4dd952f8a8f458c25bc24444e
Juan Aguilar <juan.aguilarl@unmsm.edu.pe>
2025-04-25 19:06:53 -0500

    retos 1,2,3 y 5

--- reto4.cpp
+++ reto4.cpp
@@ -2,40 +2,5 @@
 using namespace std;
 
 int main() {
-    int n;
     
-    cout << "Introduce un número impar mayor o igual a 5: ";
-    cin >> n;
-
-    if (n < 5 || n % 2 == 0) {
-        cout << "El número debe ser impar y mayor o igual a 5." << endl;
-        return 1;
-    }
-
-    int half = n / 2;
-
-    for (int i = 0; i < n; i++) {
-        if (i <= half) {
-            // Parte superior
-            for (int j = 0; j < n; j++) {
-                if (j < n - i || j >= n - i + (i)) {
-                    cout << "*";
-                } else {
-                    cout << " ";
-                }
-            }
-        } else {
-            // Parte inferior
-            for (int j = 0; j < n; j++) {
-                if (j < i + 1 || j >= i + 1 + (n - i - 1)) {
-                    cout << "*";
-                } else {
-                    cout << " ";
-                }
-            }
-        }
-        cout << endl;
-    }
-
-    return 0;
 }
